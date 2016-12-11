d5c2ef7e99bf13d998bd3c998ae0e56a0c43b9a4
chuxiuhong <nanwugongzi@vip.qq.com>
2016-12-11 20:11:28 +0800

    init

---
+++ myimage/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import  HttpResponse
+# Create your views here.
+import os
+from PIL import Image
+basedir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+def get_photo(request):
+    f1 = request.FILES['pic']
+    img = Image.open(f1)
+    img.save('current_photo.jpg')
+    return HttpResponse('i get it')
+
+def show_photo(request):
+    return render(request,'show_image.html',{})
